2e92f703d6c389db5d6be2063b7e70b14f35c485
songweek <thdwn2419@naver.com>
2024-03-04 21:11:24 +0900

    지도 기능 추가

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,15 @@ import 'package:flutter/material.dart';
 import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'Login/login_main.dart';
 import 'Home/home_main.dart';
+import 'package:flutter_naver_map/flutter_naver_map.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await NaverMapSdk.instance.initialize(
+      clientId: 'b37g6fxls3',
+      onAuthFailed: (ex) {
+        print("********* 네이버맵 인증오류 : $ex *********");
+      });
   KakaoSdk.init(nativeAppKey: 'b248c8d60ac3e422235ae6a0b0440545');
   runApp(MyApp());
 }
